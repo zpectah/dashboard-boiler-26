e452196bfe4040320101567d1eb711518a5ce426
zpecter <sychrat@gmail.com>
2026-04-04 12:54:42 +0200

    stack updates

--- src/modules/Dashboard/constants.ts
+++ src/modules/Dashboard/constants.ts
@@ -1,21 +1,32 @@
 import type { Panel } from '../../types';
 import { DirectionKeys } from './enums';
+import { dateTimeWidgetTimeKeys } from '../../enums';
 
 export const DirectionDefault = DirectionKeys.left;
 
 /** Just for temporary moment in context, before home panel is set */
 export const DashboardDefaultPanel: Panel = {
-  id: '000000',
+  id: '00000000',
   name: 'default',
-  label: 'Default',
+  label: '',
   widgets: {
-    /* TODO: tbd */
-    calendar: false,
-    clockAnalog: false,
-    clockNumeric: false,
-    dateTime: false,
-    holidays: false,
-    links: false,
-    weather: false,
+    calendar: {
+      active: false,
+    },
+    dateTime: {
+      active: false,
+      type: dateTimeWidgetTimeKeys.numeric,
+      blinkingSemi: false,
+    },
+    holidays: {
+      active: false,
+      showTomorrow: false,
+    },
+    links: {
+      active: false,
+    },
+    weather: {
+      active: false,
+    },
   },
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,52 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import { defineConfig } from 'vite';
+import react, { reactCompilerPreset } from '@vitejs/plugin-react';
+import babel from '@rolldown/plugin-babel';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
-})
+  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (
+              id.includes('@mui') ||
+              id.includes('@mui/x-date-pickers') ||
+              id.includes('@emotion/react') ||
+              id.includes('@emotion/styled')
+            )
+              return 'mui';
+            if (
+              id.includes('@tabler/icons-react') ||
+              id.includes('@fontsource/barlow')
+            )
+              return 'assets';
+            if (
+              id.includes('react-hook-form') ||
+              id.includes('@hookform/resolvers')
+            ) {
+              return 'form';
+            }
+            if (
+              id.includes('dayjs') ||
+              id.includes('i18next') ||
+              id.includes('i18next-browser-languagedetector') ||
+              id.includes('lodash') ||
+              id.includes('zod') ||
+              id.includes('zustand')
+            ) {
+              return 'utils';
+            }
+            if (
+              id.includes('react') ||
+              id.includes('react-dom') ||
+              id.includes('react-router-dom')
+            )
+              return 'react-core';
+          }
+        },
+      },
+    },
+  },
+});
